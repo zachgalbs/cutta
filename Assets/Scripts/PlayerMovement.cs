f5377b0a1b9f40662a035975ce74c4ea1df46a7d
cancano67 <ack9039@nyu.edu>
2025-11-10 18:37:58 -0500

    Working on player controls

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,49 @@
+using System.Numerics;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerMovement : MonoBehaviour
+{
+    [SerializeField] float runSpeed = 5f;
+    Animator myAnimator;
+    UnityEngine.Vector2 moveInput;
+    Rigidbody2D rb;
+    void Start()
+    {
+        myAnimator = GetComponent<Animator>();
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    void Update()
+    {
+        Run();
+        FlipSprite();
+    }
+
+    void OnMove(InputValue value)
+    {
+        moveInput = value.Get<UnityEngine.Vector2>();
+    }
+
+    void Run()
+    {
+        UnityEngine.Vector2 playerVelocity = new UnityEngine.Vector2(moveInput.x * move, rb.linearVelocity.y);
+        rb.linearVelocity = playerVelocity;
+    }
+    void FlipSprite()
+    {
+
+        bool playerHasHorizontalSpeed = Mathf.Abs(rb.linearVelocity.x) > Mathf.Epsilon;
+        if (playerHasHorizontalSpeed)
+        {
+            transform.localScale = new UnityEngine.Vector2(Mathf.Sign(rb.linearVelocity.x), 1f);
+
+        }
+
+        else
+        {
+            myAnimator.SetBool("isRunning", false);
+        }
+    }
+
+}
